4b7e2378bc6be17c4a0e8400d2944d2803726680
p1001q <p1001q@gmail.com>
2026-04-05 22:09:49 +0900

    init: 스프링 프로젝트 세팅 및 4주차 미션

---
+++ src/main/java/com/example/umc10th_spring/Umc10thSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc10th_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc10thSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc10thSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/umc10th_spring/Umc10thSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc10th_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc10thSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
