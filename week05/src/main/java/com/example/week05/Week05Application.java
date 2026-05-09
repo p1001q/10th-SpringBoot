b29d788ce29f64b193ebd8f317833873058e7401
p1001q <qrwrer123@gmail.com>
2026-05-09 19:40:37 +0900

    refactor: week05 패키지 디렉토리명 변경

--- week05/src/main/java/com/example/week05/Week05Application.java
+++ week05/src/main/java/com/example/week05/Week05Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Week04Application {
+public class Week05Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Week04Application.class, args);
