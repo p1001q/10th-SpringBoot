71a7d413a325edab633a0fc7486e04933a429eb0
p1001q <qrwrer123@gmail.com>
2026-05-11 09:40:32 +0900

    feat: 응답 통일 및 전역 예외 처리 구조 추가

---
+++ week05/src/main/java/com/example/week05/global/apiPayload/code/GeneralErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.week05.global.apiPayload.code;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum GeneralErrorCode implements BaseErrorCode {
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400_1", "잘못된 요청입니다."),           // 400: 클라이언트가 잘못된 요청을 보낸 경우
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401_1", "인증되지 않았습니다."),       // 401: 로그인 안 한 상태에서 인증이 필요한 요청을 한 경우
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403_1", "접근이 금지되었습니다."),           // 403: 로그인은 했지만 권한이 없는 경우
+    NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404_1", "해당 리소스를 찾을 수 없습니다."), // 404: 요청한 URL이 존재하지 않는 경우
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500_1", "서버 내부 오류입니다."); // 500: 서버가 예상치 못한 오류로 터진 경우
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
---
+++ week05/src/main/java/com/example/week05/global/apiPayload/exception/ProjectException.java
@@ -0,0 +1,17 @@
+package com.example.week05.global.apiPayload.exception;
+
+import com.example.week05.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ProjectException extends RuntimeException {
+
+    // 어떤 에러인지 담아두는 변수 (예: NOT_FOUND, BAD_REQUEST 등)
+    private final BaseErrorCode errorCode;
+
+    // 에러 코드를 받아서 저장하는 생성자
+    public ProjectException(BaseErrorCode errorCode) {
+        super(errorCode.getMessage()); // RuntimeException에 메시지 전달
+        this.errorCode = errorCode;
+    }
+}
