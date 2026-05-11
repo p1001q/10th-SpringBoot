93f8bbd8e9479f8ed3bf91d15c41295210d00087
p1001q <qrwrer123@gmail.com>
2026-05-11 11:41:22 +0900

    fix: 오타 수정

--- week05/src/test/java/com/example/week05/Week05ApplicationTests.java
+++ week05/src/test/java/com/example/week05/Week05ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Week04ApplicationTests {
+class Week05ApplicationTests {
 
     @Test
     void contextLoads() {
