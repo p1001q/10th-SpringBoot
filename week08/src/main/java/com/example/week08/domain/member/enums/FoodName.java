a8d45ad522faaee8777ddafe59b0bd103fe7190c
p1001q <qrwrer123@gmail.com>
2026-05-19 22:36:08 +0900

    [미션1] FoodName 음식 카테고리 추가 및 SocialType에 NONE(폼 로그인) 추가

--- week08/src/main/java/com/example/week08/domain/member/enums/FoodName.java
+++ week08/src/main/java/com/example/week08/domain/member/enums/FoodName.java
@@ -2,10 +2,16 @@ package com.example.week08.domain.member.enums;
 
 public enum FoodName {
     NONE,
-    KOREAN,
-    JAPANESE,
-    CHINESE,
-    WESTERN,
-    SNACK,
-    FAST_FOOD
+    KOREAN,       // 한식
+    JAPANESE,     // 일식
+    CHINESE,      // 중식
+    WESTERN,      // 양식
+    CHICKEN,      // 치킨
+    SNACK,        // 분식
+    GRILLED_MEAT, // 고기/구이
+    LUNCHBOX,     // 도시락
+    LATE_NIGHT,   // 야식 (족발,보쌈)
+    FAST_FOOD,    // 패스트푸드
+    DESSERT,      // 디저트
+    ASIAN_FOOD    // 아시안푸드
 }
--- week08/src/main/java/com/example/week08/domain/member/enums/SocialType.java
+++ week08/src/main/java/com/example/week08/domain/member/enums/SocialType.java
@@ -1,6 +1,7 @@
 package com.example.week08.domain.member.enums;
 
 public enum SocialType {
+    NONE,   // 폼 로그인 (이메일/비밀번호)
     KAKAO,
     NAVER,
     APPLE,
